86ba4c66983b1281c5a7407ee7967c8a3a8a25ab
rahatsikz <rascal6271@gmail.com>
2023-07-27 22:03:42 +0600

    [DONE] ~ navbar added

---
+++ src/Components/ui/Navbar.js
@@ -0,0 +1,103 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import logo from "/public/pc-high-resolution-logo-color-on-transparent-background.png";
+
+const Navbar = () => {
+  return (
+    <div className="bg-base-200 ">
+      <div className="navbar  container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <ul className="p-2 text-lg">
+                  <a>Category</a>
+                  <li>
+                    <Link href="/products/cpu">CPU</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/motherboard">Motherboard</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/ram">Ram</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/powersupply">Power Supply</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/storagedevice">Storage Device</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/monitor">Monitor</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/others">Others</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <Link href="/" className="btn btn-ghost normal-case text-xl">
+            <img src={logo.src} alt="" className="w-20" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li tabIndex={0} className="text-lg">
+              <details>
+                <summary>Category</summary>
+                <ul className="p-2">
+                  <li>
+                    <Link href="/products/cpu">CPU</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/motherboard">Motherboard</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/ram">Ram</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/powersupply">Power Supply</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/storagedevice">Storage Device</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/monitor">Monitor</Link>
+                  </li>
+                  <li>
+                    <Link href="/products/others">Others</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-primary text-white">PC Builder</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
